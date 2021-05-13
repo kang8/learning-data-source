f9aeb22c9174662164fda0a2ab9c098bf97fd1c1
kang <1115610574@qq.com>
2021-05-13 15:46:28 +0800

    customize datasource with application.yml

--- src/main/java/org/example/config/DataSourceConfig.java
+++ src/main/java/org/example/config/DataSourceConfig.java
@@ -1,20 +1,24 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    @Autowired
+    private Environment env;
 
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://121.36.19.76:3306/test?serverTimezone=GMT");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("asdf;lkj");
+        dataSourceBuilder.url(env.getProperty("datasource.customize.url"));
+        dataSourceBuilder.username(env.getProperty("datasource.customize.username"));
+        dataSourceBuilder.password(env.getProperty("datasource.customize.password"));
 
         return dataSourceBuilder.build();
     }
