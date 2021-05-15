b2c9c16b8ce9c08418ac8cda0c18a70143ee08b1
kang <1115610574@qq.com>
2021-05-15 11:01:28 +0800

    add junit5 for test

--- src/test/java/org/example/DepartmentTest.java
+++ src/test/java/org/example/DepartmentTest.java
@@ -1,24 +1,36 @@
 package org.example;
 
-import org.example.entity.Department;
-import org.example.mapper.DepartmentMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.entity.Department;
+import org.example.mapper.DepartmentMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-public class Hello {
-    public static void main(String[] args) throws Exception {
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest {
+    private DepartmentMapper departmentMapper;
+
+    @BeforeEach
+    void setUp() throws IOException {
         InputStream resource = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resource);
 
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        DepartmentMapper departmentMapper = sqlSession.getMapper(DepartmentMapper.class);
+        departmentMapper = sqlSession.getMapper(DepartmentMapper.class);
+    }
 
+    @Test
+    void testFindAll() {
         List<Department> departmentList = departmentMapper.findAll();
         departmentList.forEach(System.out::println);
     }
+
 }
