b503861cc43f4386c09fe2d644cb308633c6d0dd
kang <1115610574@qq.com>
2021-05-16 21:56:32 +0800

    feat: add user entity

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,80 @@
+package org.example.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class User {
+    private String id;
+
+    private String name;
+
+    private Integer age;
+
+    private LocalDateTime birthday;
+
+    private Department department;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age) && Objects.equals(birthday, user.birthday) && Objects.equals(department, user.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, birthday, department);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                ", department=" + department +
+                '}';
+    }
+}
